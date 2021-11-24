c8e3aa4af73e863a4db5ac6d858ddb61338acf3d
Jakub Jirkal <jakub.jirkal.020@pslib.cz>
2021-11-24 19:10:00 +0100

    4th

---
+++ java1st/script.js
@@ -0,0 +1,24 @@
+
+
+function openImg(n){
+
+    document.getElementById("k1").style.display = "none";
+    document.getElementById("k2").style.display = "none";
+    document.getElementById("k3").style.display = "none";
+    document.getElementById("k4").style.display = "none";
+    document.getElementById("k5").style.display = "none";
+    document.getElementById("k6").style.display = "none";
+    document.getElementById("k7").style.display = "none";
+
+    document.getElementById(n).style.display = "block";
+
+    document.getElementById('gallery').style.display = "none";
+
+}
+
+function closeImg(n){
+    
+    document.getElementById(n).style.display = "none";
+    document.getElementById('alert').style.display = "none";
+    document.getElementById('gallery').style.display = "grid";
+}
